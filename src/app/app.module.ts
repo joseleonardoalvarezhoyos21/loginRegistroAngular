77302e95dd9c00a15f58e45821f6440eea7cc35f
unknown <josealvarez1654@outlook.com>
2023-06-11 18:44:18 -0500

    Login arreglar animación de registro y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginRegistroComponent } from './componentes/login-registro/login-registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    LoginRegistroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/login-registro/login-registro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-registro',
+  templateUrl: './login-registro.component.html',
+  styleUrls: ['./login-registro.component.css']
+})
+export class LoginRegistroComponent {
+
+}
